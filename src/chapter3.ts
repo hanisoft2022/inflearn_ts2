b213d7c5964472b1210f8817b17d9528abc8f0c9
hani <hanisoft2022@gmail.com>
2025-09-16 20:14:54 +0900

    chore: chapter3

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -1,4 +1,11 @@
 
 // object
-let user: { id: number, name: string } = { id: 1, name: '김장한' };
+let user: { id?: number, name: string } = { id: 1, name: '김장한' };
 
+console.log(user.id);
+
+user = { name: '김한장' };
+
+let config: { readonly apiKey: string } = { apiKey: "MY API KEY" };
+
+// config.apiKey = "MY API KEY2";
